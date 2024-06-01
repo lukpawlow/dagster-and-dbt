cbf397e9dee23fef4b92796d9f8f54e6847f7bfe
lukpawlow <lukpawlow@gmail.com>
2024-06-01 19:12:25 +0200

    lesson 6 complated

--- dagster_university/assets/dbt.py
+++ dagster_university/assets/dbt.py
@@ -1,17 +1,28 @@
-from dagster import AssetExecutionContext
-from dagster_dbt import dbt_assets, DbtCliResource
-
 import os
-
-from pathlib import Path
-
-from ..resources import dbt_resource
+import json
+from dagster import AssetExecutionContext, AssetKey
+from dagster_dbt import dbt_assets, DbtCliResource, DagsterDbtTranslator
 
 from .constants import DBT_DIRECTORY
+from ..resources import dbt_resource
+from ..partitions import daily_partition
 
-# dbt_resource.cli(["--quiet", "parse"], target_path=Path("target")).wait()
+INCREMENTAL_SELECTOR = "config.materialized:incremental"
 
-#ex dbt_manifest_path = DBT_DIRECTORY.joinpath("target", "manifest.json")
+class CustomizedDagsterDbtTranslator(DagsterDbtTranslator):
+    def get_asset_key(self, dbt_resource_props):
+        resource_type = dbt_resource_props["resource_type"]
+        name = dbt_resource_props["name"]
+        if resource_type == "source":
+            return AssetKey(f"taxi_{name}")
+        else:
+            return super().get_asset_key(dbt_resource_props)
+        
+    def get_group_name(self, dbt_resource_props):
+        return dbt_resource_props["fqn"][1]
+
+
+dbt_resource.cli(["--quiet", "parse"]).wait() # to chyba jest niepotrzebne
 
 if os.getenv("DAGSTER_DBT_PARSE_PROJECT_ON_LOAD"):
     dbt_manifest_path = (
@@ -22,9 +33,25 @@ else:
     dbt_manifest_path = os.path.join(DBT_DIRECTORY, "target", "manifest.json")
 
 
-
 @dbt_assets(
     manifest=dbt_manifest_path,
+    dagster_dbt_translator=CustomizedDagsterDbtTranslator(),
+    exclude=INCREMENTAL_SELECTOR,
 )
 def dbt_analytics(context: AssetExecutionContext, dbt: DbtCliResource):
     yield from dbt.cli(["build"], context=context).stream()
+
+
+@dbt_assets(
+    manifest=dbt_manifest_path,
+    dagster_dbt_translator=CustomizedDagsterDbtTranslator(),
+    select=INCREMENTAL_SELECTOR,     # select only models with INCREMENTAL_SELECTOR
+    partitions_def=daily_partition,   # partition those models using daily_partition
+)
+def incremental_dbt_models(context: AssetExecutionContext, dbt: DbtCliResource):
+    time_window = context.partition_time_window
+    dbt_vars = {
+        "min_date": time_window.start.strftime('%Y-%m-%d'),
+        "max_date": time_window.end.strftime('%Y-%m-%d')
+    }    
+    yield from dbt.cli(["build", "--vars", json.dumps(dbt_vars)], context=context).stream()
